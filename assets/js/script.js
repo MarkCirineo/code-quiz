a2a79a298974bcd014e6d8252ae9090b66571ad1
Mark Cirineo <88806942+MarkCirineo@users.noreply.github.com>
2021-09-11 14:35:10 -0400

    added question one on button press with basic styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,16 @@ var startButton = document.querySelector("#start-button");
 var startP = document.querySelector("#start-p")
 var h1 = document.querySelector("h1")
 
-startButton.addEventListener("click", questionTwo)
+startButton.addEventListener("click", startQuestionOne)
 
-function questionTwo() {
+function startQuestionOne() {
     startP.setAttribute("style", "display: none;");
     h1.setAttribute("style", "display: none;");
     startButton.setAttribute("style", "display: none;")
+
+    var questionOne = document.createElement("h2")
+    questionOne.textContent = "Commonly used data types DO NOT include: "
+    document.body.children[1].appendChild(questionOne);
+
+
 }
