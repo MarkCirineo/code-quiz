7efed1c69e5e6ac4ac618750592d7367baa3655a
Mark Cirineo <88806942+MarkCirineo@users.noreply.github.com>
2021-09-12 14:48:22 -0400

    added basic function for timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,11 +8,29 @@ var questionTwo = document.getElementById("question-two");
 var questionThree = document.getElementById("question-three");
 var questionFour = document.getElementById("question-four");
 var questionFive = document.getElementById("question-five");
+var time = 60;
+
+//timer function
+function timer() {
+    var allTimer = setInterval(function() {
+        time--;
+        if (time === 0 || time < 0) {
+            clearInterval(allTimer);
+            console.log("lose")
+        }   
+    }, 1000);
+}
+
+function wrongAnswer() {
+    time = time - 10;
+    console.log(time)
+}
 
 //clicking the start button
 startButton.addEventListener("click", startQuestionOne);
- 
+
 function startQuestionOne() {
+    timer();
     //got rid of starting screen
     startP.setAttribute("style", "display: none;");
     h1.setAttribute("style", "display: none;");
@@ -26,6 +44,7 @@ function startQuestionOne() {
 
 //function for wrong alert for all question
 function wrongAlertAll() {
+    wrongAnswer();
     var wrongAlert = document.createElement("h3");
     wrongAlert.textContent = "Wrong!"
     main.appendChild(wrongAlert);
